ff24e28aaab587737216f8c443be56327dea1ccb
ajdehn <ajdehn76@gmail.com>
2021-10-14 20:13:06 -0500

    Create rdsAllRegions.py

---
+++ rdsAllRegions.py
@@ -0,0 +1,22 @@
+import boto3
+import json
+
+def main():
+    # Pre requisite - ensure you have RDS Read Access
+    available_regions = boto3.Session().get_available_regions('rds')
+    # Loop through all regions
+    for region in available_regions:
+        try:
+            rdsClient = boto3.client('rds', region_name=region)
+            allDatabases = rdsClient.describe_db_instances()
+            for db in allDatabases['DBInstances']:
+                print('%s: %s' % (region, db['DBInstanceIdentifier']))
+            # Save all regions file for all databases
+            fileName = 'rds_regions/%s.json' % region
+            with open(fileName, 'w') as f:
+                json.dump(allDatabases, f, indent=4, default=str)
+        except Exception as e:
+            if 'InvalidClientTokenId' in e.response['Error']['Code']:
+                pass
+            else:
+                print(e)
